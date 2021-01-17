ab513547397ab99f5810d58864fb862d92988622
Fernan <fernanhick@gmail.com>
2021-01-17 19:32:28 +0000

    Added HeroElements and button for all layout

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,12 +1,44 @@
-import React from 'react';
-import {HeroBg, HeroiContainer, VideoBg} from './HeroElements';
-
+import React, {useState} from 'react';
+import {
+  ArrowForward,
+  ArrowRight,
+  HeroBg,
+  HeroBtnWrapper,
+  HeroContent,
+  HeroH1,
+  HeroiContainer,
+  HeroP,
+  VideoBg,
+} from './HeroElements';
+import Video from '../../videos/video.mp4';
+import {Button} from '../ButtonElement';
 const HeroSection = () => {
+  const [hover, setHover] = useState(false);
+  const onHover = () => {
+    setHover(!hover);
+  };
   return (
     <HeroiContainer>
       <HeroBg>
-        <VideoBg></VideoBg>
+        <VideoBg autoPlay loop muted src={Video} type="video/mp4"></VideoBg>
       </HeroBg>
+      <HeroContent>
+        <HeroH1>Defi Options Made Easy</HeroH1>
+        <HeroP>
+          Sign in for a overview of the Dashboard for DeFi options in the market
+        </HeroP>
+        <HeroBtnWrapper>
+          <Button
+            to="signup"
+            onMouseEnter={onHover}
+            onMouseLeave={onHover}
+            primary="true"
+            dark="true"
+          >
+            Get Started {hover ? <ArrowForward /> : <ArrowRight />}
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
     </HeroiContainer>
   );
 };
